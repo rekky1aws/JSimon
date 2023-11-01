43a3741cda026d4ffaff7861644e4ab4002586da
rekky1aws <killikomfr@gmail.com>
2023-11-01 05:37:48 +0100

    Sounds
    On click, different sounds for each button.
    Comments for the future.

--- script/app.js
+++ script/app.js
@@ -1,13 +1,26 @@
 // Constants
 const btns = document.querySelectorAll('.simon_button');
 
+// Variables
+let count = 1;
+let gameList = [];
+
 // EventListeners
 btns.forEach( (btn) => {
 	btn.addEventListener('click', pressedBtn);
+	count++;
 });
 
 // Functions
 function pressedBtn (e)
 {
 	let color = e.target.id;
+
+	// if correct choice
+	let sound = new Audio("/media/sounds/" + color + ".wav");
+	sound.play();
 }
+
+// generateNextChoice
+
+// playGameList
